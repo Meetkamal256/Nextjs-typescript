c2aeb90f1c12c2acb00526c73d9dbd859920de90
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-06-14 21:50:37 +0100

    modified notFound page

---
+++ nextjs-tut/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NOtFound() {
+  return (
+    <div>
+      <h2>Page not found</h2>
+      <p>Could not find required resource</p>
+    </div>
+  );
+}
--- nextjs-tut/src/app/products/reviews/[reviewId]/page.tsx
+++ nextjs-tut/src/app/products/reviews/[reviewId]/page.tsx
@@ -1,11 +1,19 @@
-export default function ReviewDetail({
-  params,
-}: {
+import { notFound } from "next/navigation";
+
+type ReviewDetailProps = {
   params: {
     productId: string;
     reviewId: string;
   };
-}) {
+};
+
+export default function ReviewDetail({ params }: ReviewDetailProps) {
+  const reviewId = parseInt(params.reviewId, 10);
+
+  if (reviewId > 1000) {
+    notFound();
+  }
+
   return (
     <h1>
       Review {params.reviewId} for product {params.productId}
