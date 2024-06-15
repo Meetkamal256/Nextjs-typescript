7846ac287d3e0899161d3c40bae40cd1be4ce7ec
Muhammad Kamaluddeen <meetkamal256@gmail.com>
2024-06-15 12:06:40 +0100

    nested layout s in next js app router

---
+++ nextjs-tut/src/app/products/[productId]/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ProductDetailLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      <h2>Featured Products</h2>
+    </>
+  );
+}
